b3c26a1225273cb8481f3ed53f57e9cb7603a030
Jovanie Cabatuan <jhovaniecabatuan@gmail.com>
2026-06-19 16:16:55 +0800

    initialized project

---
+++ src/main/java/jcss/soft/com/authorized_proxy/AuthorizedProxyApplication.java
@@ -0,0 +1,13 @@
+package jcss.soft.com.authorized_proxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthorizedProxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthorizedProxyApplication.class, args);
+	}
+
+}
