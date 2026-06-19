d67d8829aaaa24f30c1ef4dae8f309c982e546de
Jovanie Cabatuan <jhovaniecabatuan@gmail.com>
2026-06-19 20:26:32 +0800

    add actuator

--- src/test/java/jcss/soft/com/authorized_proxy/AuthorizedProxyApplicationTests.java
+++ src/test/java/jcss/soft/com/authorized_proxy/AuthorizedProxyApplicationTests.java
@@ -1,13 +1,13 @@
-package jcss.soft.com.authorized_proxy;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class AuthorizedProxyApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package jcss.soft.com.authorized_proxy;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class AuthorizedProxyApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
